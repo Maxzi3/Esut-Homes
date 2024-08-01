c497867e1a47daa6b9a6d1a7e5911f557a301f3c
Maxzi3 <obisikemaxwell@gmail.com>
2024-08-01 12:36:04 +0100

    images issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,24 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "custom-bg": "url('./Esutlodgesbg.jpg')",
-        "bg2": "url('./bg1.jpg')",
-        "bg3": "url('./bg3.jpg')",
-        
-        
+        "custom-bg": "url('/Esutlodgesbg.jpg')",
+        bg2: "url('/bg1.jpg')",
+        bg3: "url('/bg3.jpg')",
       },
       colors: {
         primary: "#fbe3d1",
         secondary: "#640000",
       },
-       keyframes: {
+      keyframes: {
         fadeInBottom: {
-          '0%': { opacity: 0, transform: 'translateY(100%)' },
-          '100%': { opacity: 1 },
+          "0%": { opacity: 0, transform: "translateY(100%)" },
+          "100%": { opacity: 1 },
         },
       },
       animation: {
-        fadeInBottom: 'fadeInBottom 1s ease-out',
+        fadeInBottom: "fadeInBottom 1s ease-out",
       },
+    },
   },
-},
   plugins: [],
 };
