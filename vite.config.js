25791581131d8abf095c1d2b31a941dd4a2d306a
Maxzi3 <obisikemaxwell@gmail.com>
2024-08-01 12:17:54 +0100

    deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react-countup"],
+  },
   build: {
     rollupOptions: {
-      external: ['react-countup']
-    }
+      external: ["react-countup"],
+    },
   },
-  server :{
-    port: 3000
-  }
-})
+  server: {
+    port: 3000,
+  },
+});
