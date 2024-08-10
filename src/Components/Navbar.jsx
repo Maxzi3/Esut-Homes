12b2b5dce48f6d8e5b0d81253990a08f720f3c4b
Maxzi3 <obisikemaxwell@gmail.com>
2024-08-10 20:01:45 +0100

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
   };
 const linkclass = ({ isActive }) =>
   isActive
-    ? ' mr-4 hover:text-gray-900 overline'
-    : 'mr-4 hover:text-gray-900 ';
+    ? " mr-4 hover:text-gray-900 overline px-2 py-1"
+    : "mr-4 hover:text-gray-900 px-2 py-1";
   return (
     <div>
       <header className="fixed top-0 left-0 right-0 z-50 text-primary body-font w-full bg-secondary">
@@ -31,10 +31,40 @@ const linkclass = ({ isActive }) =>
               )}
             </button>
           </div>
+
+          {/* Mobile view menu links */}
+          <nav
+            className={`${
+              isOpen ? "max-h-44 opacity-100" : "max-h-0 opacity-0"
+            } overflow-hidden transition-all duration-300 ease-in-out md:hidden flex flex-col md:flex-row items-end text-base justify-center w-full md:w-auto`}
+          >
+            <NavLink to="/" className={linkclass} onClick={toggleMenu}>
+              Home
+            </NavLink>
+            <NavLink to="/About" className={linkclass} onClick={toggleMenu}>
+              About
+            </NavLink>
+            <NavLink to="/Lodges" className={linkclass} onClick={toggleMenu}>
+              Lodges
+            </NavLink>
+            <NavLink to="/AddLodges" className={linkclass} onClick={toggleMenu}>
+              Add Lodges
+            </NavLink>
+            <a
+              href="https://portal.esut.edu.ng/login.aspx"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center bg-primary border-0 py-1 px-3 focus:outline-none hover:bg-secondary hover:text-primary text-secondary rounded text-base mt-4 mr-4 md:mt-0"
+            >
+              My Portal
+            </a>
+          </nav>
+
+          {/* Desktop menu links */}
           <nav
             className={`${
               isOpen ? "block" : "hidden"
-            } md:flex flex-col md:flex-row items-center text-base justify-center w-full md:w-auto`}
+            } hidden md:flex flex-col md:flex-row items-center text-base justify-center w-full md:w-auto`}
           >
             <NavLink to="/" className={linkclass} onClick={toggleMenu}>
               Home
